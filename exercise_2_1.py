a163dd285392c51c0d995c7f463180444a942873
subrockmann <50804225+subrockmann@users.noreply.github.com>
2020-11-04 12:43:05 +0100

    feat: exercise 2.1.b - 2.1.d

--- exercise_2_1.py
+++ exercise_2_1.py
@@ -34,12 +34,33 @@ if np.allclose(T_AB, T_AB_lec):  # check for approximate equality of the two arr
     print("T_AB:")
     print_result(T_AB)
 
-T_BC = np.dot(trans(np.array((-4,-1,0)).transpose()), rot2trans(rotz(np.radians(270))))
+T_BC = np.matmul(trans(np.array((-4,-1,0)).transpose()), rot2trans(rotz(np.radians(270))))
 if np.allclose(T_BC, T_BC_lec): 
     print("T_BC:")
     print_result(T_BC)
 
-T_AC = np.dot(trans(np.array((2,1,0)).transpose()), rot2trans(rotz(np.radians(90))))
+T_AC = np.matmul(trans(np.array((2,1,0)).transpose()), rot2trans(rotz(np.radians(90))))
 if np.allclose(T_AC, T_AC_lec): 
     print("T_AC:")
     print_result(T_AC)
+
+# exercise 2.1.b
+print("Exercise 2.1.b")
+T_AC_calc = np.matmul(T_AB,T_BC)
+if np.allclose(T_AC_calc, T_AC): 
+    print("T_AC:")
+    print_result(T_AC)
+
+# exercise 2.1.c
+print("Exercise 2.1.c")
+I = np.matmul(np.linalg.inv(T_AC), T_AC)
+if np.allclose(I ,np.identity(4)):
+    print("I:")
+    print_result(I)
+
+# exercise 2.1.d
+print("Exercise 2.1.d")
+p_B = np.array((-3,1, 0, 1)).transpose()
+p_A = np.matmul(T_AB, p_B)
+print("p_A:")
+print_result(p_A)
