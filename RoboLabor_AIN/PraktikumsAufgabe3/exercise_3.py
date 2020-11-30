cbe8242cbe9b4061e53c4f6a0728e9d4c7b5d598
subrockmann <50804225+subrockmann@users.noreply.github.com>
2020-11-30 13:44:46 +0100

    feat: implemented gotoGlobal function

--- RoboLabor_AIN/PraktikumsAufgabe3/exercise_3.py
+++ RoboLabor_AIN/PraktikumsAufgabe3/exercise_3.py
@@ -27,7 +27,7 @@ myRobot._k_d = 0 #0.05 * 0.05  # velocity noise parameter = 0.05m*0.05m / 1m
 myRobot._k_theta = 0 # (5.0 * 5.0/360.0) * (pi/180.0)  # turning rate noise parameter = 5deg*5deg/360deg * (1rad/1deg)
 myRobot._k_drift = 0 # (2.0 * 2.0)/1.0 * (pi/180.0)**2  # drift noise parameter = 2deg*2deg / 1m
 
-myWorld.setRobot(myRobot, [12, myWorld._height/2, 0])
+
 
 # KeyboardController definieren:
 myKeyboardController = myWorld.getKeyboardController()
@@ -79,16 +79,56 @@ def turnOnSpot(robot, theta, omega):
             robot.move(motion)
         return robot
 
+def rectangleDrive():
+    straightDrive(myRobot, 1,25 )
+    turnOnSpot(myRobot, np.radians(90), 1.2)
+    straightDrive(myRobot, 1,25 )
+    turnOnSpot(myRobot, np.radians(90), 0.5)
+    straightDrive(myRobot, 1,25 )
+    turnOnSpot(myRobot, np.radians(90), 0.5)
+    straightDrive(myRobot, 1,25 )
+    turnOnSpot(myRobot, np.radians(90), 0.5)
+    return
 
-straightDrive(myRobot, 1,25 )
-turnOnSpot(myRobot, np.radians(90), 1.2)
-straightDrive(myRobot, 1,25 )
-turnOnSpot(myRobot, np.radians(90), 0.5)
-straightDrive(myRobot, 1,25 )
-turnOnSpot(myRobot, np.radians(90), 0.5)
-straightDrive(myRobot, 1,25 )
-turnOnSpot(myRobot, np.radians(90), 0.5)
-#straightDrive(myRobot, 0.5, 80)
+def angularDifference(theta_1, theta_2):
+    return np.mod((theta_1 - theta_2 + np.pi), 2*np.pi) - np.pi
+
+def gotoGlobal(robot, v, p, tol):
+    K_omega = 0.1   # macht der Wert Sinn?
+    K_v = 0.1
+    v_max = robot._maxSpeed
+    omega_max = robot._maxOmega
+    (x_star, y_star) = p
+
+    # determine distance to final position
+    (x,y,theta) = myWorld.getTrueRobotPose()
+    delta = np.sqrt((x_star - x)**2 + (y_star - y)**2)
+    myWorld.addBox(x_star,y_star)
+
+    while delta > tol:     
+
+        print(f"x: {x}, y: {y}")
+        theta_star = atan2(y_star -y, x_star - x)
+        theta_delta  = angularDifference(theta_star, theta)
+        print(f"delta theta: {theta_delta}")
+        omega = float(np.minimum(omega_max, K_omega * theta_delta, casting='same_kind')) 
+        print(f"Omega: {omega}, {type(omega)} v: {v}, {type(v)}")
+        v_t = np.minimum(v_max, v)
+        v_t = np.minimum(v_t, K_v * (np.sqrt((x_star - x)**2 + (y_star - y)**2)))
+        print(v_t)
+        # move the robot for 1 timestep
+        robot.move([v_t, omega])
+
+        # determine remaining distance to final position
+        (x,y,theta) = myWorld.getTrueRobotPose()
+        delta = np.sqrt((x_star - x)**2 + (y_star - y)**2)
+
+
+    return robot
+
+myWorld.setRobot(myRobot, [12, myWorld._height/2, 2])
+gotoGlobal(myRobot, 0.5, (23, 50), 1)
+#straightDrive(myRobot, 0.5, 30)
 
 
 
